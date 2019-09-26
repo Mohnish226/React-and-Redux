f2bb57d5ddbeff50331300c7af8c9cdb0ec50efb
Mohnish Devadiga <mohnish226@yahoo.co.in>
2019-09-27 03:53:46 +1000

    Update App.js

--- 02-bmi-app/src/App.js
+++ 02-bmi-app/src/App.js
@@ -3,22 +3,14 @@ import './App.css';
 
 class App extends Component{
 
-  
+    render() {
+        return(
+            <div>
+                <p>test</p>
+            </div>
+        )
+    }
 
-}
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
 }
 
 export default App;
